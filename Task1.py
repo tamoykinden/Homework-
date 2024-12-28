8ad89f9166a15a7ef2a3d9a1fca03ad0904bd452
Denis Tamoykin <denistamoykin04@gmail.com>
2024-12-28 19:03:09 +0300

    Add class Student

---
+++ Task1.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
