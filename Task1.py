376e0d4b3ae9e6ba3af8f3683a49ecb6ce2a903b
Denis Tamoykin <denistamoykin04@gmail.com>
2024-12-28 19:38:49 +0300

    Add class Reviewer

--- Task1.py
+++ Task1.py
@@ -22,3 +22,6 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
